1a4fbca82881b3eb81620200b93b48275e2ab1f0
Administrator <2667134364@qq.com>
2021-08-15 13:58:53 +0800

    master

---
+++ Date_Type/demo_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=1;
+    char b='b';
+    printf("a是：%d \n",a);
+    printf("b是: %c",b);
+    return 0;
+}
---
+++ Date_Type/tou/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "tou.h"
+ 
+int main(void)
+{
+    printf("add:%d\n", add(2, 5));
+    printf("sub:%d\n", sub(5, 2));
+    return 0;
+}
